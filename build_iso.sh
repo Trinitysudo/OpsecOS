7154ba86ab0cfcfb8a7d3e1b82b996db5f0bf3b5
Silva <dexterpardox@gmail.com>
2026-02-07 15:31:31 +1400

    Initial OpsecOS Build Structure

---
+++ build_iso.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# OpsecOS Build Script
+# Run this on an Arch Linux system with 'archiso' installed.
+
+set -e
+
+# Cleanup previous builds
+echo "Cleaning up..."
+rm -rf work out
+
+# Startup
+echo "Starting OpsecOS Build..."
+mkdir -p out
+sudo mkarchiso -v -w work -o out .
+
+echo "Build complete! Your ISO is in the 'out' directory."
+ls -lh out
---
+++ profiledef.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# profiledef.sh
+
+iso_name="opsecos"
+iso_label="OPSEC_OS_$(date +%Y%m)"
+iso_publisher="OpsecOS Project <https://github.com/opsecos>"
+iso_application="OpsecOS Live/Installation Media"
+iso_version="$(date +%Y.%m.%d)"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
+           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
+           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/usr/local/bin/opsec-live-setup"]="0:0:755"
+  ["/etc/calamares/modules"]="0:0:755"
+  ["/etc/sysctl.d/99-security.conf"]="0:0:644"
+  ["/etc/dnscrypt-proxy/dnscrypt-proxy.toml"]="0:0:644"
+)
